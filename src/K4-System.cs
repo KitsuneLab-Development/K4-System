10bca7b5e85ec58f03794ad6193cff562694a870
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2023-11-16 22:16:39 +0100

    fix: Unload problems for non-existing player

--- src/K4-System.cs
+++ src/K4-System.cs
@@ -20,32 +20,39 @@ namespace K4ryuuSystem
 		{
 			Log($"{ModuleName} [{ModuleVersion}] by {ModuleAuthor} is starting to load.", LogLevel.Debug, hotReload);
 
-			MySql = new MySqlDb(Config.DatabaseSettings.Host, Config.DatabaseSettings.Username, Config.DatabaseSettings.Password, Config.DatabaseSettings.Database, Config.DatabaseSettings.Port);
-
-			Log("Database connection established successfully.", LogLevel.Debug, hotReload);
-
-			MySql.ExecuteNonQueryAsync(@$"CREATE TABLE IF NOT EXISTS `{TablePrefix}k4ranks` (`id` INT AUTO_INCREMENT PRIMARY KEY, `steam_id` VARCHAR(32) UNIQUE NOT NULL, `name` VARCHAR(255) NOT NULL, `rank` VARCHAR(255) NOT NULL, `points` INT NOT NULL DEFAULT 0, UNIQUE (`steam_id`));");
-			MySql.ExecuteNonQueryAsync(@$"CREATE TABLE IF NOT EXISTS `{TablePrefix}k4times` (`id` INT AUTO_INCREMENT PRIMARY KEY, `steam_id` VARCHAR(32) UNIQUE NOT NULL, `name` VARCHAR(255) NOT NULL, `all` INT NOT NULL DEFAULT 0, `ct` INT NOT NULL DEFAULT 0, `t` INT NOT NULL DEFAULT 0, `spec` INT NOT NULL DEFAULT 0, `dead` INT NOT NULL DEFAULT 0, `alive` INT NOT NULL DEFAULT 0);");
-			MySql.ExecuteNonQueryAsync(@$"CREATE TABLE IF NOT EXISTS `{TablePrefix}k4stats` (`id` INT AUTO_INCREMENT PRIMARY KEY, `steam_id` VARCHAR(32) UNIQUE NOT NULL, `name` VARCHAR(255) NOT NULL, `kills` INT NOT NULL DEFAULT 0, `deaths` INT NOT NULL DEFAULT 0, `hits` INT NOT NULL DEFAULT 0, `headshots` INT NOT NULL DEFAULT 0, `grenades` INT NOT NULL DEFAULT 0, `mvp` INT NOT NULL DEFAULT 0, `round_win` INT NOT NULL DEFAULT 0, `round_lose` INT NOT NULL DEFAULT 0);");
-
-			LoadRanksFromConfig();
-
-			if (hotReload)
+			try
 			{
-				List<CCSPlayerController> players = Utilities.GetPlayers();
+				MySql = new MySqlDb(Config.DatabaseSettings.Host, Config.DatabaseSettings.Username, Config.DatabaseSettings.Password, Config.DatabaseSettings.Database, Config.DatabaseSettings.Port);
 
-				foreach (CCSPlayerController player in players)
+				Log("Database connection established successfully.", LogLevel.Debug, hotReload);
+
+				MySql.ExecuteNonQueryAsync(@$"CREATE TABLE IF NOT EXISTS `{TablePrefix}k4ranks` (`id` INT AUTO_INCREMENT PRIMARY KEY, `steam_id` VARCHAR(32) UNIQUE NOT NULL, `name` VARCHAR(255) NOT NULL, `rank` VARCHAR(255) NOT NULL, `points` INT NOT NULL DEFAULT 0, UNIQUE (`steam_id`));");
+				MySql.ExecuteNonQueryAsync(@$"CREATE TABLE IF NOT EXISTS `{TablePrefix}k4times` (`id` INT AUTO_INCREMENT PRIMARY KEY, `steam_id` VARCHAR(32) UNIQUE NOT NULL, `name` VARCHAR(255) NOT NULL, `all` INT NOT NULL DEFAULT 0, `ct` INT NOT NULL DEFAULT 0, `t` INT NOT NULL DEFAULT 0, `spec` INT NOT NULL DEFAULT 0, `dead` INT NOT NULL DEFAULT 0, `alive` INT NOT NULL DEFAULT 0);");
+				MySql.ExecuteNonQueryAsync(@$"CREATE TABLE IF NOT EXISTS `{TablePrefix}k4stats` (`id` INT AUTO_INCREMENT PRIMARY KEY, `steam_id` VARCHAR(32) UNIQUE NOT NULL, `name` VARCHAR(255) NOT NULL, `kills` INT NOT NULL DEFAULT 0, `deaths` INT NOT NULL DEFAULT 0, `hits` INT NOT NULL DEFAULT 0, `headshots` INT NOT NULL DEFAULT 0, `grenades` INT NOT NULL DEFAULT 0, `mvp` INT NOT NULL DEFAULT 0, `round_win` INT NOT NULL DEFAULT 0, `round_lose` INT NOT NULL DEFAULT 0);");
+
+				LoadRanksFromConfig();
+
+				if (hotReload)
 				{
-					if (player.IsBot)
-						continue;
+					List<CCSPlayerController> players = Utilities.GetPlayers();
 
-					LoadPlayerData(player);
+					foreach (CCSPlayerController player in players)
+					{
+						if (player.IsBot)
+							continue;
+
+						LoadPlayerData(player);
+					}
 				}
+
+				SetupGameEvents();
+
+				Log($"{ModuleName} [{ModuleVersion}] by {ModuleAuthor} has been loaded.", LogLevel.Debug, hotReload);
 			}
-
-			SetupGameEvents();
-
-			Log($"{ModuleName} [{ModuleVersion}] by {ModuleAuthor} has been loaded.", LogLevel.Debug, hotReload);
+			catch (Exception ex)
+			{
+				Log($"Error while loading: {ex.Message}", LogLevel.Error);
+			}
 		}
 
 		public override void Unload(bool hotReload)
@@ -61,16 +68,19 @@ namespace K4ryuuSystem
 					if (player.IsBot)
 						continue;
 
-					if (Config.GeneralSettings.ModuleTimes)
-						SaveClientTime(player);
+					if (PlayerSummaries.ContainsPlayer(player))
+					{
+						if (Config.GeneralSettings.ModuleTimes)
+							SaveClientTime(player);
 
-					if (Config.GeneralSettings.ModuleStats)
-						SaveClientStats(player);
+						if (Config.GeneralSettings.ModuleStats)
+							SaveClientStats(player);
 
-					if (Config.GeneralSettings.ModuleRanks)
-						SaveClientRank(player);
+						if (Config.GeneralSettings.ModuleRanks)
+							SaveClientRank(player);
 
-					PlayerSummaries.RemovePlayer(player);
+						PlayerSummaries.RemovePlayer(player);
+					}
 				}
 
 				Log($"{ModuleName} [{ModuleVersion}] by {ModuleAuthor} has been unloaded. Database saved.", LogLevel.Debug);
