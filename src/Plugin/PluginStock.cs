b5db345a9e5b614908b066d22c9f39d9c12b66bb
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2023-12-28 20:40:24 +0100

    refactor: Remove unused functions

--- src/Plugin/PluginStock.cs
+++ src/Plugin/PluginStock.cs
@@ -10,28 +10,6 @@ namespace K4System
 
 	public sealed partial class Plugin : BasePlugin
 	{
-		public string ApplyPrefixColors(string msg)
-		{
-			if (!msg.Contains('{'))
-			{
-				return string.IsNullOrEmpty(msg) ? $"{ChatColors.LightRed}[K4-System]" : msg;
-			}
-
-			string modifiedValue = msg;
-			Type chatColorsType = typeof(ChatColors);
-
-			foreach (FieldInfo field in chatColorsType.GetFields())
-			{
-				string pattern = $"{{{field.Name}}}";
-				if (modifiedValue.Contains(pattern, StringComparison.OrdinalIgnoreCase))
-				{
-					modifiedValue = modifiedValue.Replace(pattern, field.GetValue(null)!.ToString(), StringComparison.OrdinalIgnoreCase);
-				}
-			}
-
-			return modifiedValue;
-		}
-
 		public bool InitializeDatabase(string tableName, string createTableQuery)
 		{
 			tableName = $"{this.Config.DatabaseSettings.TablePrefix}{tableName}";
