e5880082e3f0b454ea9641e5c00172f9e30ba1ee
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-01-22 22:40:45 +0100

    chore: Change version

--- src/Plugin/PluginManifest.cs
+++ src/Plugin/PluginManifest.cs
@@ -10,7 +10,7 @@ namespace K4System
 
         public override string ModuleAuthor => "K4ryuu";
 
-        public override string ModuleVersion => "3.0.4 " +
+        public override string ModuleVersion => "3.1.0 " +
 #if RELEASE
             "(release)";
 #else
