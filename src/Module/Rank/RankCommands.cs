0ec6b0fc5a37d140620efa661087d05e141c1a4e
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-01-26 17:37:54 +0100

    refactor: Change Top Max to 25 with Clamp instead

--- src/Module/Rank/RankCommands.cs
+++ src/Module/Rank/RankCommands.cs
@@ -125,7 +125,7 @@ namespace K4System
 
 					if (int.TryParse(info.ArgByIndex(1), out int parsedInt))
 					{
-						printCount = Math.Max(1, Math.Min(50, parsedInt));
+						printCount = Math.Clamp(parsedInt, 1, 25);
 					}
 
 					CCSPlayerController savedPlayer = player;
