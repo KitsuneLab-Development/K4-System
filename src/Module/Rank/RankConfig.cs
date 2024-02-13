5e171f403cb2da73d8845610c237f82c77b31b07
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-02-13 10:18:41 +0100

    feat: Add all CS ranks to default config

--- src/Module/Rank/RankConfig.cs
+++ src/Module/Rank/RankConfig.cs
@@ -12,35 +12,119 @@ namespace K4System
 			string ranksFilePath = Path.Join(ModuleDirectory, "ranks.jsonc");
 
 			string defaultRanksContent = @"{
-	""None"": { // Whatever you set here, be unique. Not read by plugin
-		""Name"": ""None"", // This name is set in MySQL and also if not Tag is preset, this is the Tag
-		""Point"": -1, // Whatever you set to -1 is the default rank
+	""None"": {
+		""Name"": ""None"",
+		""Point"": -1,
 		""Color"": ""default""
 	},
-	""Silver"": {
-		""Name"": ""Silver"",
-		""Tag"": ""[S]"", // Clan tag (scoreboard) of the rank. If not set, it uses the key instead, which is currently ""Silver""
-		""Point"": 250, // From this amount of experience, the player is Silver
-		""Color"": ""lightblue"" // Color code for the rank. Find color names here: https://github.com/roflmuffin/CounterStrikeSharp/blob/main/managed/CounterStrikeSharp.API/Modules/Utils/ChatColors.cs
+	""Silver I"": {
+		""Name"": ""Silver I"",
+		""Tag"": ""[SI]"",
+		""Point"": 100,
+		""Color"": ""silver""
 	},
-	""Gold"": {
-		""Name"": ""Gold"",
-		""Tag"": ""[G]"",
-		""Point"": 1000,
-		""Color"": ""red"",
-		""Permissions"": [ // You can add permissions to the rank. If you don't want to add any, remove this line
-			{
-				""DisplayName"": ""Super Permission"", // This is the name of the permission. Will be displayed in the menu of ranks to let people know the benefits of a rank
-				""PermissionName"": ""permission1"" // This is the permission name. You can assign 3rd party permissions here
-			},
-			{
-				""DisplayName"": ""Legendary Permission"",
-				""PermissionName"": ""permission2""
-			}
-			// You can add as many as you want
-		]
+	""Silver II"": {
+		""Name"": ""Silver II"",
+		""Tag"": ""[SII]"",
+		""Point"": 500,
+		""Color"": ""silver""
+	},
+	""Silver III"": {
+		""Name"": ""Silver III"",
+		""Tag"": ""[SIII]"",
+		""Point"": 900,
+		""Color"": ""silver""
+	},
+	""Silver IV"": {
+		""Name"": ""Silver IV"",
+		""Tag"": ""[SIV]"",
+		""Point"": 1300,
+		""Color"": ""silver""
+	},
+	""Silver Elite"": {
+		""Name"": ""Silver Elite"",
+		""Tag"": ""[SE]"",
+		""Point"": 1700,
+		""Color"": ""silver""
+	},
+	""Silver Elite Master"": {
+		""Name"": ""Silver Elite Master"",
+		""Tag"": ""[SEM]"",
+		""Point"": 2100,
+		""Color"": ""silver""
+	},
+	""Gold Nova I"": {
+		""Name"": ""Gold Nova I"",
+		""Tag"": ""[GNI]"",
+		""Point"": 2600,
+		""Color"": ""gold""
+	},
+	""Gold Nova II"": {
+		""Name"": ""Gold Nova II"",
+		""Tag"": ""[GNII]"",
+		""Point"": 3100,
+		""Color"": ""gold""
+	},
+	""Gold Nova III"": {
+		""Name"": ""Gold Nova III"",
+		""Tag"": ""[GNIII]"",
+		""Point"": 3600,
+		""Color"": ""gold""
+	},
+	""Gold Nova Master"": {
+		""Name"": ""Gold Nova Master"",
+		""Tag"": ""[GNM]"",
+		""Point"": 4100,
+		""Color"": ""gold""
+	},
+	""Master Guardian I"": {
+		""Name"": ""Master Guardian I"",
+		""Tag"": ""[MGI]"",
+		""Point"": 4700,
+		""Color"": ""green""
+	},
+	""Master Guardian II"": {
+		""Name"": ""Master Guardian II"",
+		""Tag"": ""[MGII]"",
+		""Point"": 5300,
+		""Color"": ""green""
+	},
+	""Master Guardian Elite"": {
+		""Name"": ""Master Guardian Elite"",
+		""Tag"": ""[MGE]"",
+		""Point"": 5900,
+		""Color"": ""green""
+	},
+	""Distinguished Master Guardian"": {
+		""Name"": ""Distinguished Master Guardian"",
+		""Tag"": ""[DMG]"",
+		""Point"": 6500,
+		""Color"": ""green""
+	},
+	""Legendary Eagle"": {
+		""Name"": ""Legendary Eagle"",
+		""Tag"": ""[LE]"",
+		""Point"": 7200,
+		""Color"": ""blue""
+	},
+	""Legendary Eagle Master"": {
+		""Name"": ""Legendary Eagle Master"",
+		""Tag"": ""[LEM]"",
+		""Point"": 7900,
+		""Color"": ""blue""
+	},
+	""Supreme Master First Class"": {
+		""Name"": ""Supreme Master First Class"",
+		""Tag"": ""[SMFC]"",
+		""Point"": 8600,
+		""Color"": ""purple""
+	},
+	""Global Elite"": {
+		""Name"": ""Global Elite"",
+		""Tag"": ""[GE]"",
+		""Point"": 9300,
+		""Color"": ""lightred""
 	}
-	// You can add as many as you want
 }";
 
 			if (!File.Exists(ranksFilePath))
