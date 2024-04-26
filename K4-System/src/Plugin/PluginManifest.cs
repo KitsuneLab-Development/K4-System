d49948fb1090786b0a01cb856b567260e75a3fe7
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-04-26 11:43:46 +0200

    CSS updated

--- K4-System/src/Plugin/PluginManifest.cs
+++ K4-System/src/Plugin/PluginManifest.cs
@@ -10,7 +10,7 @@ namespace K4System
 
         public override string ModuleAuthor => "K4ryuu";
 
-        public override string ModuleVersion => "4.1.7 " +
+        public override string ModuleVersion => "4.1.8 " +
 #if RELEASE
             "(release)";
 #else
