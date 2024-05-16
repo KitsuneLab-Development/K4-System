2f0cf2ce36dcca997ca52c6b042bbf0e7d6dcc2a
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-05-16 21:34:07 +0200

    feat: Update module version to 4.3.0

--- K4-System/src/Plugin/PluginManifest.cs
+++ K4-System/src/Plugin/PluginManifest.cs
@@ -10,7 +10,7 @@ namespace K4System
 
         public override string ModuleAuthor => "K4ryuu";
 
-        public override string ModuleVersion => "4.2.2 " +
+        public override string ModuleVersion => "4.3.0 " +
 #if RELEASE
             "(release)";
 #else
